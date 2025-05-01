d10cebd911eb89322b3ebdba81f4eed0e900b2d9
Yuankai619 <Yuankai_@outlook.com>
2025-05-01 20:39:11 +0800

    Configure typography plugin

--- src/app/wiki/[wikiSlug]/page.tsx
+++ src/app/wiki/[wikiSlug]/page.tsx
@@ -24,13 +24,26 @@ export default async function WikiPage({
     });
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 py-10">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-50 text-zinc-900 px-4 py-10">
             <h1 className="text-3xl font-bold mb-4">
                 {data.frontmatter.title}
             </h1>
-            <article className="prose prose-blue max-w-3xl w-full">
-                {data.content}
-            </article>
+            <article className="prose max-w-3xl w-full">{data.content}</article>
+            {/* <article className="prose prose-xl">
+                <h1>This is Title</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Impedit obcaecati temporibus delectus et eaque non enim,
+                    consequatur illum velit sapiente molestiae soluta
+                    voluptatibus omnis quasi dolores maxime officiis at vero!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Aut dignissimos quasi pariatur nobis ipsa ullam! Commodi
+                    modi, saepe eveniet soluta numquam quasi ducimus, corrupti
+                    architecto distinctio dignissimos alias nesciunt doloribus?
+                </p>
+            </article> */}
         </div>
     );
 }
